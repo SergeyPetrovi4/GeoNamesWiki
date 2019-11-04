139c53ddafe563e247c56c88e91204a875c2b099
Sergey Krasiuk <>
2019-11-04 12:37:43 +0200

    Init commit

---
+++ GeoNamesWiki/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoNamesWiki
+//
+//  Created by Sergey Krasiuk on 04/11/2019.
+//  Copyright © 2019 Sergey Krasiuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
